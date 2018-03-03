89ee3e82da3bd774afcf4bfa0ff01be7fc9095c2
Mosegaard Erik <in17moer@lwb.ch>
2018-03-03 20:34:37 +0100

    em
    em

--- Abfrage.sql
+++
@@ -1,1 +0,0 @@
---Hier kommen alle abfrage statements herein!
---
+++ Scripts.sql/[inf17]_[alpha2]_00_create-db.sql
@@ -0,0 +1,10 @@
+USE [master]
+GO
+
+/****** Create Database [DAlpha]******/
+CREATE DATABASE [DAlpha];
+GO
+
+/****** Datenbank verwenden******/
+USE [DAlpha];
+GO
